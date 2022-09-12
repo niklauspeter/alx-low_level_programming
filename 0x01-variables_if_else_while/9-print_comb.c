ad879ab15b4879d74008813bab44961c738bf47a
niklauspeter <oriokiklaus@gmail.com>
2022-09-12 16:35:46 -0700

    printssingle digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints all single digit numbers
+ * adds comma and space after each
+ * Return:0 if exits ok
+*/
+
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+putchar(i);
+if (i != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
