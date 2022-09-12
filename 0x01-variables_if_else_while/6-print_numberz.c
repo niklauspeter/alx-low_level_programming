b5a5fe12071701801ce2a455110fd8fe1450a15f
niklauspeter <oriokiklaus@gmail.com>
2022-09-12 15:52:24 -0700

    print single base numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main-prints single digit numbers of base 10
+ * Return:0 if properly exits
+*/
+
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
