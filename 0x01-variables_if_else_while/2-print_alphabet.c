84cceb36380b925f543711a6ab565ab09f8a88f5
niklauspeter <oriokiklaus@gmail.com>
2022-09-12 14:12:09 -0700

    print lower case alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main-prints outlower case leters
+ * Return:0 if properly exits
+*/
+
+int main(void)
+{
+	char lower;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+	putchar(lower);
+	putchar('\n');
+	}
+	return (0);
+}
+
