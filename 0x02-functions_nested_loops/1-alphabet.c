05bf26edd5f59b340197794ebdb0ff9265df2964
niklauspeter <oriokiklaus@gmail.com>
2022-09-14 15:41:18 -0700

    add function that prints alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function thatprints alphabet
+*/
+
+void print_alphabet(void);
+{
+int c;
+for (c = 'a'; c <= 'z'; ++c)
+_putchar(c);
+_putchar('\n');
+}
