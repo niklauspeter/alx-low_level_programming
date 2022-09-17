3a832fc293a8781cf80580576b80b01dbece9ab2
niklauspeter <oriokiklaus@gmail.com>
2022-09-16 19:18:25 -0700

    debugging tst

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - tests function which prints if int is pos or negative
+ * Return: 0 if propely exits
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
+
