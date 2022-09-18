8a0086e35cccbbbb70506b6e37567f40f7577e66
niklauspeter <oriokiklaus@gmail.com>
2022-09-18 09:05:15 -0700

    checks if input is single digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if input is a single digit no
+ * @c: placeholder for argument
+ * Return: 1 if digit and 0 if not
+*/
+
+int _isdigit(int c)
+{
+	int i = 0;
+	int digit = 0;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (c == i)
+		{
+			digit = 1;
+			break;
+		}
+	}
+	return (digit);
+}
