5085cbc73b65cf67bff394d72becd6e379529750
niklauspeter <oriokiklaus@gmail.com>
2022-09-19 15:18:33 -0700

    fundciton updates value it points to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes pointer and updates the value to 98
+ * Return:0
+ * @*n: pointer parameter
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+first on pointers and string
