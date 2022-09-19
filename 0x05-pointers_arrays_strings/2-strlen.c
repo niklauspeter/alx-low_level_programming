e5da80317d1fcbdac0fe22c0d11a1d017c7b808e
niklauspeter <oriokiklaus@gmail.com>
2022-09-19 15:49:09 -0700

    counts and returns legth of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns legthof string
+ * @s: takes string as input
+ * Return: value of string
+*/
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\10')
+		length++;
+	return (length);
+}
