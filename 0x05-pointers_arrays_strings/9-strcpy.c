ff7f9f193f8ba28ffe7f8ba6ff9618f33998a580
niklauspeter <oriokiklaus@gmail.com>
2022-09-19 18:25:49 -0700

    funciton copies string poointed to by src to buffer pointed to by dest

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies a string pointed to by src to buffer pointed to by dest
+ * @dest: where string should be copied to
+ * @src: ource origin of string
+ * Return: pointer string copied todest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	char *str = dest;
+
+	while (*src)
+		*dest++ = *src++;
+	return (str);
+}
