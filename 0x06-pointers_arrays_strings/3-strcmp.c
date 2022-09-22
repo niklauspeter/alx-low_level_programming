afd10f11300a61a1f9f65abb8e8769c03ee90e41
niklauspeter <oriokiklaus@gmail.com>
2022-09-22 12:31:35 -0700

    function compares two strings
    "
    :Q
    Q
    "
    ""
    ";
    "

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp - function compares two strings
+ * @s1: input takes in string
+ * @s2: input takes in string
+ * Return: difference s1 and s2:
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++, s2++;
+	}
+	return (*s1 - *s2);
+}
