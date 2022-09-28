986f8a4fff2f99136d4f8d701bb4823f6fa7af85
niklauspeter <oriokiklaus@gmail.com>
2022-09-28 11:48:46 -0700

    copies n bytes from one array to another

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies n bytes from src to dest
+ * @dest : area to be copied into
+ * @src : array to be copied from
+ * @n: memory bytes to be copied
+ * Return: returns pointer to dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *start = dest;
+	int i = 0;
+
+	while (i < n)
+	{
+		*dest = *src;
+		++dest;
+		++src;
+	}
+	return (start);
+}
