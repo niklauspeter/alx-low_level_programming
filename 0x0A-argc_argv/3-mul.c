1449997af839a29945e2a5e8941fa0b9619dee45
niklauspeter <oriokiklaus@gmail.com>
2022-10-05 05:22:50 -0700

    program prints multiple of 2 numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program prints out the multiple of two numbers
+ * @argc : numberof arguments
+ * @argv : value of arguments in string
+ * Return: 0 if successful
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{
+		int sum;
+
+		sum = atoi(argv[1]) + atoi(argv[2]);
+		printf("%d\n", sum);
+		return (0);
+	}
+	else
+	{
+		printf("error\n");
+		return (1);
+	}
+}
