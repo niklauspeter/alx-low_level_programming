93978f581f638208e03b24f7fd62a307e2c6b7fc
niklauspeter <oriokiklaus@gmail.com>
2022-10-12 08:24:14 -0700

    program prints duplicate of string input into new string with allocated memomry

---
+++ 0x0B-malloc_free/1-srdup.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _strdup - function returns pointer to new string
+ * new string is duplicate of input string
+ * @str: input string
+ * Return: NULL if string is null and
+ * returns pointer to duplicated string if successful
+*/
+
+char *_strdup(char *str)
+{
+	char *new_str;
+	char *start;
+	int i = 0, len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	start = str;
+
+	while (*str)
+	{
+		len++;
+		str++;
+	}
+
+	str = start;
+	new_str = (char *)malloc(sizeof(char) * (len + 1));
+	start = new_str;
+
+	if (str != NULL)
+	{
+		for (i = 0; i < len; i++)
+		{
+			new_str[i] = *str;
+			str++;
+		}
+		new_str[i] = '\0';
+		return (start);
+	}
+	else
+		return (NULL);
+}
