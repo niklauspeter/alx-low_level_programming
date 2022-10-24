01635cd5d15373a6cfef35dd4aebdf6796cca856
niklauspeter <oriokiklaus@gmail.com>
2022-10-24 15:17:34 -0700

    correct description

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
-/*using contructors to execute code before main 
- * funciton runs
-*void main_constructor(void) __attribute__((constructor));
+/**
  * main_constructor - function print message before the
  * main function runs
  * Return:nothing
 */
-void __attribute__ ((constructor)) main_contructor(void)
+void __attribute__ ((constructor)) main_constructor(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
