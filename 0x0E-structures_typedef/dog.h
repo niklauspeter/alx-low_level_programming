afc6e3aec274d2ac1c1c36509a3ef7b5b8514811
niklauspeter <oriokiklaus@gmail.com>
2022-10-27 06:15:22 -0700

    create header with struct called dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_
+#define _DOG_
+/**
+ * struct dog - takes in name age and owner
+ * @name: takes in name as input
+ * @age: takes in age as input
+ * @owner: takes in owner as input
+*/
+
+struct dog{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*_DOG_*/
