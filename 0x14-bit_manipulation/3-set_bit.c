8f8d021204d4519acbfb6b662a147c3a1a48f9c6
niklauspeter <oriokiklaus@gmail.com>
2022-11-01 16:37:21 -0700

    fnction sets a bit at given index to 1

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,10 +2,18 @@
 
 /**
  * set_bit - function sets value ofa bit to 1 at a given index
- * @n - pointer to int number
+ * @n: pointer to int number
  * @index: index to replace starting from 0
+ * Return: 1 if it works or -1 on error
 */
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int max = 0x01;
 
+	max <<= index;
+	if (max == 0)
+		return (-1);
+	*n |= max;
+	return (1);
+}
