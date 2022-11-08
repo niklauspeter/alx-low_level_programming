a37a5d39d4394a598cf60af0e0b323bd20e9bf38
niklauspeter <oriokiklaus@gmail.com>
2022-11-08 07:22:32 -0800

    add header

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,6 +1,6 @@
+#include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
-#include "variadic_functions.h"
 
 /**
  * sum_them_all - variadic function that returns the
@@ -13,15 +13,13 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
+	unsigned int i = 0;
+	int sum = 0;
 
 	if (n == 0)
 		return (0);
 
-	unsigned int i = 0;
-	int sum = 0;
-
 	va_start(ap, n);
-
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(ap, int);
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,7 +3,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-char _putchar(char c);
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 
 #endif /*_VARIADIC_H_*/
