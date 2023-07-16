764d1ce9374cb12e9bb0d31b4e8cb5f971ec1494
klaus <oriokiklaus@gmail.com>
2023-07-16 20:33:03 +0300

    add description for binary search function

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * binary_search - function searches for value in a sorted array of integers
+ * _binary_search - function searches for value in a sorted array of integers
  * it uses the binary search algorithm
  * @array: array of integers
  * @left: Left element
