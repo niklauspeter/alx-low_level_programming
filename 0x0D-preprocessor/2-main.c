5bda06a6afc0053c9c2b4d13a8fe8e815b3fe4a1
klaus <oriokiklaus@gmail.com>
2023-09-23 15:18:09 +0300

    program prints name of file it was compiled form

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+main - function prints name of file it 
+* was compliled from
+Return: returns 0 if successful
+*/
+
+int main(void)
+{
+    printf("%s\n", __FILE__);
+    return (0);
+}
